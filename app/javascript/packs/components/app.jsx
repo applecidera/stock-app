6f8886f99af7b25b709e66e8323347522473795a
Andrew <leea2142@gmail.com>
2020-02-21 11:16:22 -0500

    created landing page

--- app/javascript/packs/components/app.jsx
+++ app/javascript/packs/components/app.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+// import { AuthRoute, ProtectedRoute } from '../utils/route_utils';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './landing_page/landing_page';
 
 class App extends React.Component{
   render() {
     return (
-      <div>
-        Stock Portfolio App
-      </div>
+      // <Switch>
+      //   <ProtectedRoute path="/" component={LandingPage} />
+      //   <AuthRoute path="/" component={LandingPage} />
+      // </Switch>
+      <LandingPage />
     )
   }
 }
