50f1158a9f27db73bccbb8b9b114a342bc7d5f87
Andrew <leea2142@gmail.com>
2020-02-27 12:06:45 -0500

    jquery not working on heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-//= require jquery
-//= require rails-ujs
-//= require activestorage
-//= require_tree .
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
